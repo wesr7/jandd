5794e7cbe53ad0276d49b54d85e80426a05595b7
wesr7 <jwesleyrodgers@gmail.com>
2015-08-28 23:10:34 -0700

    added instagram images to main page. changed some colors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
     def main
+        @endpoint="https://api.instagram.com/v1/tags/thejuanfordanielle/media/recent?access_token=10081932.64d472e.266dfc56a7c842ce9f18e45b4feee8b1"
+
+        @response = HTTParty.get(@endpoint)
+
+        @images = []
+
+        @response['data'].each do |element|
+            @images << element['images']['low_resolution']['url']
+        end
+        @images
     end
 
 end
